d0753758d7f4c43e56e4a7229e1214c6bc6bf777
rajeshdutta <rajesh.dutta@nmbrsystems.com>
2022-02-02 14:11:04 +0530

    Demo Web App With Apache Jasper and Controller

---
+++ MyWebApp/src/main/java/com/rajesh/demo/HomeController.java
@@ -0,0 +1,15 @@
+package com.rajesh.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/home")
+	
+	public String home() {
+		System.out.println("OK");
+		return "home.jsp";
+	}
+}
